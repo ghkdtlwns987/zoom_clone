b6f47ba7e1d916a63cacc00b93173612f2cc8b77
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-01-10 02:15:42 +0900

    v3

---
+++ backend/server copy.js
@@ -0,0 +1,42 @@
+/*!
+ *
+ * WebRTC Lab
+ * @author dodortus (dodortus@gmail.com / codejs.co.kr)
+ *
+ */
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('backend/private.pem'),
+  cert: fs.readFileSync('backend/public.pem')
+};
+
+const config = require('./config.json');
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+//const http = require('http').Server(app);
+const http = require('http').Server(app);
+const port = process.env.PORT || config.webserver.port;
+const root = `${__dirname}/../`;
+const path = {
+  frontend: `${root}/frontend`,
+};
+
+
+app.set('views', path.frontend + '/views');
+app.engine('ejs', ejs.renderFile);
+app.use(express.static(path.frontend + '/contents'));
+app.use(express.static(path.frontend + '/views/examples'));
+
+// Routes
+require('./routes')(app);
+
+// Socket.io
+require('./controllers/socket.js')(http);
+//https.createServer(options, app).listen(3000);
+
+// Server listen
+http.listen(port, function () {
+  console.log('WebRTC Lab server running at ' + config.webserver.host + ':' + port);
+});
