3bdb9e46dbedf0acf67eec80e7ab05bd727b078b
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-01-12 13:41:35 +0900

    2023-01-12

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -5,58 +5,8 @@ module.exports = [
     renderFile: 'index.ejs',
   },
   {
-    title: 'WebRTC 소개',
-    url: '/intro',
-    renderFile: 'intro.ejs',
-  },
-  {
     title: '카메라 테스트',
     url: '/get-user-media',
     renderFile: 'examples/get-user-media/index.ejs',
-  },
-  {
-    title: '비디오에 필터 적용하기',
-    url: '/filter',
-    renderFile: 'examples/filter/index.ejs',
-  },
-  {
-    title: '비디오를 이미지로 캡쳐하기',
-    url: '/capture',
-    renderFile: 'examples/capture/index.ejs',
-  },
-  {
-    title: '로그인',
-    url: '/login',
-    renderFile: 'examples/login/index.ejs',
-  },
-  {
-    title: '영상통화',
-    url: '/conference',
-    renderFile: 'examples/conference/index.ejs',
-  },
-  {
-    title: '화면 공유하기',
-    url: '/screen-share',
-    renderFile: 'examples/screen-share/index.ejs',
-  },
-  {
-    title: '파일 전송하기',
-    url: '/data-channel',
-    renderFile: 'examples/data-channel/index.ejs',
-  },
-  {
-    title: '음성 인식(STT)',
-    url: '/speech-recognition',
-    renderFile: 'examples/speech-recognition/index.ejs',
-  },
-  {
-    title: '멀티 스트림',
-    url: '/multi-stream',
-    renderFile: 'examples/multi-stream/index.ejs',
-  },
-  {
-    title: '다이나믹 레졸루션',
-    url: '/dynamic-resolution',
-    renderFile: 'examples/dynamic-resolution/index.ejs',
   },
 ];
